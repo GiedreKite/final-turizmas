8ac274573be00741cd0a0d17dd47cd1b104046da
GiedreKite <giedre.narvilaite@gmail.com>
2024-09-10 15:54:24 +0300

    up

---
+++ client/src/data/locationsData.js
@@ -0,0 +1,39 @@
+import img1 from '../assets/locations/1.png';
+import img2 from '../assets/locations/2.png';
+import img3 from '../assets/locations/3.png';
+
+export const locationsData = [
+    {
+        img: img1,
+        name: 'Menulis',
+        address: {
+            country: 'Kostmosas',
+            city: 'A',
+            street: 'A',
+            number: 'A',
+            zip: 'A',
+        },
+    },
+    {
+        img: img2,
+        name: 'Gelyte',
+        address: {
+            country: 'Tundra',
+            city: 'B',
+            street: 'B',
+            number: 'B',
+            zip: 'B',
+        },
+    },
+    {
+        img: img3,
+        name: 'Meduza',
+        address: {
+            country: 'Baltijos jura',
+            city: 'C',
+            street: 'C',
+            number: 'C',
+            zip: 'C',
+        },
+    },
+];
