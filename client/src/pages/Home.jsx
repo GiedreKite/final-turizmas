3ea18a3e3caeca584ec73eaf3a4942782d78f6a6
GiedreKite <giedre.narvilaite@gmail.com>
2024-09-15 01:34:52 +0300

    up

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { HomeHero } from "../components/hero/HomeHero";
 
 export function Home() {
     return (
-        <>
+        <><h1>Turinys:</h1>
             <Header />
             <HomeHero />
             <Footer />
